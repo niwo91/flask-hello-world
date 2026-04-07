4809940a90c526e507cac670c87676c21ab47583
Nick <niwo9128@colorado.edu>
2026-04-07 11:03:54 -0600

    Add Required routes

--- app.py
+++ app.py
@@ -28,5 +28,51 @@ def db_create():
     ''')
     conn.commit()
     conn.close()
-    return "Basketball Table Created Succesfully"
+    return "Basketball Table Succesfully Created"
 
+
+@app.route('/db_insert')
+def db_test():
+    conn = psycopg2.connect("postgresql://nick:ZwHsLj5AY0n4bZxrIFxL8zZAPUt4DDBD@dpg-d7aj19fkijhs73dodnk0-a/nickdb_lvej")
+    cur.execute('''                                                                                                                          
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+    ''')                                                                                                                                     
+    conn.commit() 
+    conn.close()
+    return "Basketball Table Succesfully Populated"
+
+
+@app.route('/db_select')
+def db_test():
+    conn = psycopg2.connect("postgresql://nick:ZwHsLj5AY0n4bZxrIFxL8zZAPUt4DDBD@dpg-d7aj19fkijhs73dodnk0-a/nickdb_lvej")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball
+        ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string=""
+    response_string+="<table>"
+    for player in records:
+        response_string+="<tr>"
+        for info in player:
+            response_string+=f"<td>{info}</td>"
+        response_string+="</tr>"
+    response_string+="</table>"
+    return response_string
+
+@app.route('/db_drop')
+def db_test():
+    conn = psycopg2.connect("postgresql://nick:ZwHsLj5AY0n4bZxrIFxL8zZAPUt4DDBD@dpg-d7aj19fkijhs73dodnk0-a/nickdb_lvej")
+    cur = conn.cursor()
+    cur.execute('''
+        DROP TABLE Basketball;
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Dropped"
