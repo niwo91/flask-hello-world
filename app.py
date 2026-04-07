c036cb8a155b09534348f268ffe9442d5aa4e832
Nick <niwo9128@colorado.edu>
2026-04-07 10:32:28 -0600

    Add database and requirements

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+import psycopg2
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Nicholas Woody in 3308'
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgresql://nick:ZwHsLj5AY0n4bZxrIFxL8zZAPUt4DDBD@dpg-d7aj19fkijhs73dodnk0-a/nickdb_lvej")
+
+    conn.close()
+    return "Database Connection Successful"
